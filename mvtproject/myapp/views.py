f862a955073f02e4741672ccae6ac2a3fd74841f
Pavan <tpavankumar158@gmail.com>
2026-07-18 12:44:07 +0530

    Views Added

--- mvtproject/myapp/views.py
+++ mvtproject/myapp/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect,get_object_or_404
+from myapp.models import Product
+from myapp.forms import ProductForm
 # Create your views here.
+def addproduct(request):
+    pform = ProductForm()
+    if request.method == 'POST':
+        pform = ProductForm(request.POST)
+        if pform.is_valid():
+            pform.save(commit=True)
+            return redirect('/')
+    return render(request,'myapp/add.html',{'form':pform}) 
+
+def getproducts(request):
+    products = Product.objects.all()
+    return render(request,'myapp/products.html',{'PrdList':products})   
+     
+def getproduct(request):
+    pid=request.GET.get('id')
+    product = get_object_or_404(Product,ProductId=pid)
+    return render(request,'myapp/find.html',{'prds':product})
+
+def editproduct(request,id):
+    product = get_object_or_404(Product,ProductId=id)
+    pform = ProductForm(instance=product)
+    if request.method=='POST':
+        pform = ProductForm(request.POST,instance=product)
+        if pform.is_valid():
+            pform.save()
+            return redirect('/')
+    return render(request,'myapp/edit.html',{'form':pform})
+
+def deleteproduct(request,id):
+    product = get_object_or_404(Product,ProductId=id)
+    if request.method =="POST":
+        product.delete()
+        return redirect('/')
+    return render(request,'myapp/delete.html',{'prds':product})
