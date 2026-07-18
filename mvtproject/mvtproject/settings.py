0a4d5477ce1ff77ec7f809bc45059fa9917911d8
Pavan <tpavankumar158@gmail.com>
2026-07-18 11:36:05 +0530

    Admin Added

--- mvtproject/mvtproject/settings.py
+++ mvtproject/mvtproject/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'myapp',
+    
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'mvtproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'mvtproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mvtpracdb',
+        'USER':'root',
+        'PASSWORD':'pavan158',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
--- mvtproject/myapp/admin.py
+++ mvtproject/myapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from myapp.models import Employee
 # Register your models here.
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display=["EmployeeId","EmployeeName","EmployeeSalary"]
+admin.site.register(Employee,EmployeeAdmin)
+
+
